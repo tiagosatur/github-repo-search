abddee6a731aae58a24c59e58139791f69a4c83f
Satur <tiagosatur@gmail.com>
2023-04-05 21:14:28 -0300

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Github repo search</h1>
+    </div>
+  );
+}
+
+export default App;
